e6d603a9ffaf1d60907e56fc5dcd48064d1b398f
Vladimir Shevchenko <prstud@yandex.ru>
2024-01-20 01:35:01 +0300

    создал приложения tasks, comments, добавил настройки isort в pyproject.toml pre-commit hook

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "src.apps.comments",
     "src.apps.lms",
+    "src.apps.tasks",
     "src.apps.users",
 ]
 
--- src/apps/lms/models.py
+++ src/apps/lms/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+import time
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db import models
+
+User = get_user_model()
+
+
+class F(models):
+    pass
+
+
+class CUser(AbstractUser):
+    try:
+        pass
+    except ValidationError:
+        pass
+    ...
+
+
+time = time.time()
