939f227894954eacad21c2ccdc6873aa30d3552d
Korkin Maxim <splintermax92@yandex.ru>
2024-01-22 13:24:00 +0300

    TaskreadSerializer + TaskViewSet + Router(urls)

--- src/apps/api_v1/views.py
+++ src/apps/api_v1/views.py
@@ -1,3 +1,21 @@
-# from django.shortcuts import render
+from rest_framework import viewsets
+from serializers import TaskSerializer
 
-# Create your views here.
+from tasks.models import Task
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        # в работе
+        serializer.save()
+
+    def perform_update(self, serializer):
+        # в работе
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # в работе
+        instance.delete()
