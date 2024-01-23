848c4bf037170d292a10c53de49b7861abfcad17
Andey Dogadkin <jvgger@gmail.com>
2024-01-23 15:23:42 +0300

    Added users serializers.

---
+++ src/apps/users/serializers.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from src.apps.lms.models import LMS
+
+CustomUser = get_user_model()
+
+
+class CustomUserListSerializer(serializers.ModelSerializer):
+    """
+    Вывод списка пользователей с активным ИПР если такой существует.
+    """
+
+    active_lms = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "id",
+            "first_name",
+            "second_name",
+            "last_name",
+            "position",
+            "active_lms",
+            "photo",
+        )
+        read_only_fields = "__all__"
+
+    def get_active_lms(self, obj: CustomUser):
+        """
+        Получить активный ИПР пользователя.
+        """
+        return LMS.objects.filter(employee=obj, is_active=True).last()
+
+
+class CustomUserRetrieveSerializer(serializers.ModelSerializer):
+    """
+    Вывод пользователя со всеми принадлежащими ему ИПР.
+    """
+
+    lms = ...  # ShortDataLMSSerializer(many=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "id",
+            "first_name",
+            "second_name",
+            "last_name",
+            "position",
+            "grade",
+            "role",
+            "photo",
+            "lms",
+        )
+        read_only_fields = "__all__"
