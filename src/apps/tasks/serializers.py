d1093be24bfe0cf7428fd914c7ab703b2ba7cfd9
Korkin Maxim <splintermax92@yandex.ru>
2024-01-23 21:07:36 +0300

    Task serializer v2

---
+++ src/apps/tasks/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from tasks.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="name")
+    description = serializers.CharField(max_length=512)
+    deadline = serializers.DateField(read_only=True)
+    date_added = serializers.DateField(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "name",
+            "description",
+            "deadline",
+            "status",
+            "date_added",
+            "lms",
+        ]
+
+    def get_user(self):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+        return user
