ea31844482822c2fa75de2f28b154f2d6085b8ce
Andey Dogadkin <jvgger@gmail.com>
2024-01-23 23:07:23 +0300

    Added django filters.

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "django_filters",
     "src.apps.comments",
     "src.apps.lms",
     "src.apps.tasks",
