fe6a686223db68fab32cfc6f84ea0222e4e7d199
Andey Dogadkin <jvgger@gmail.com>
2024-01-25 15:15:20 +0300

    Fix CommentSerializer validate method.

--- src/apps/comments/serializers.py
+++ src/apps/comments/serializers.py
@@ -1,6 +1,9 @@
+from django.db.models import Q
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from src.apps.comments.models import Comment
+from src.apps.tasks.models import Task
 
 
 class CommentSerializer(ModelSerializer):
@@ -20,8 +23,17 @@ class CommentSerializer(ModelSerializer):
         )
 
     def validate(self, attrs):
-        attrs["comment_author"] = self.context["request"].user
-        attrs["task_id"] = self.context["request"].parser_context["kwargs"][
-            "task_id"
-        ]
+        task_id = self.context["request"].parser_context["kwargs"]["task_id"]
+        comment_author = self.context["request"].user
+        task = Task.objects.select_related("lms").filter(
+            Q(id=task_id, lms__supervisor=comment_author)
+            | Q(id=task_id, lms__employee=comment_author)
+        )
+        if not task:
+            raise serializers.ValidationError(
+                detail="You are not allowed to leave a comment on a "
+                "task that you are not related to."
+            )
+        attrs["comment_author"] = comment_author
+        attrs["task_id"] = task_id
         return attrs
