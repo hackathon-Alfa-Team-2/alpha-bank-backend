6e9dfd99de75aa052932152193832fb41aa82c2d
Andey Dogadkin <jvgger@gmail.com>
2024-01-26 04:07:31 +0300

    Fixed lms app urls.

--- src/apps/lms/urls.py
+++ src/apps/lms/urls.py
@@ -4,7 +4,11 @@ from rest_framework.routers import DefaultRouter
 from src.apps.lms.views import LMSViewSet
 
 lms_router_v1 = DefaultRouter()
-lms_router_v1.register("lms", LMSViewSet)
+lms_router_v1.register(
+    r"users/(?P<user_id>\d+)/lms",
+    LMSViewSet,
+    basename="lms",
+)
 
 urlpatterns = [
     path("", include(lms_router_v1.urls)),
