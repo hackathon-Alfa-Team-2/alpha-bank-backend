c3c65625dcc6a052b75ffdc233a455bc5f05ea0e
Andey Dogadkin <jvgger@gmail.com>
2024-01-26 04:08:27 +0300

    Added get_queryset_method to LMSViewSet.

--- src/apps/lms/views.py
+++ src/apps/lms/views.py
@@ -7,10 +7,20 @@ from src.base.permissions import IsAdminOrSupervisorOrLMSExecutor
 
 
 class LMSViewSet(viewsets.ModelViewSet):
-    queryset = LMS.objects.all()
     serializer_class = FullDataLMSSerializer
     permission_classes = [IsAuthenticated, IsAdminOrSupervisorOrLMSExecutor]
 
     def perform_create(self, serializer):
         supervisor = self.request.user
         serializer.save(supervisor=supervisor)
+
+    def get_queryset(self):
+        user = self.request.user
+        url_user = self.kwargs.get("user_id")
+        if user.is_supervisor:
+            return LMS.objects.select_related("supervisor").filter(
+                supervisor=user, employee_id=url_user
+            )
+        if user.role.name == "employee":
+            return LMS.objects.select_related("employee").filter(employee=user)
+        return LMS.objects.all()
