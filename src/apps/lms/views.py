67695c0759b3ffd4c961edaac16679b1f9b6fd9b
vbamnup <vbamnup@gmail.com>
2024-01-28 17:43:42 +0300

    Add LMS statistic: - Total count LMS - Number of LMSs with a deadline in the current month - Total number of completed LMSs

--- src/apps/lms/views.py
+++ src/apps/lms/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from django.db.models import Count, Case, When
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from src.apps.lms.models import LMS
-from src.apps.lms.serializers import FullDataLMSSerializer
+from src.apps.lms.serializers import FullDataLMSSerializer, StatisticSerializer
 from src.base.permissions import IsAdminOrSupervisorOrLMSExecutor
+from src.base.utils import get_current_month_range
 
 
 class LMSViewSet(viewsets.ModelViewSet):
@@ -24,3 +28,28 @@ class LMSViewSet(viewsets.ModelViewSet):
                 employee=user,
             )
         return LMS.objects.all()
+
+    @action(
+        url_name="statistic",
+        detail=False,
+        methods=["get"],
+        permission_classes=[IsAuthenticated],
+    )
+    def statistic(self, request, *args, **kwargs):
+        month_start, month_end = get_current_month_range()
+        stats_data = LMS.objects.aggregate(
+            total_count=Count("id"),
+            deadlines_this_month=Count(
+                Case(
+                    When(
+                        deadline__gte=month_start,
+                        deadline__lte=month_end,
+                        then=1,
+                    )
+                )
+            ),
+            completed_count=Count(Case(When(status="completed", then=1))),
+        )
+        serializer = StatisticSerializer(data=stats_data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
