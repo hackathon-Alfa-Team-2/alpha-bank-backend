cfeb123a169c96ba875608e442cd25bc2feb64c0
Korkin Maxim <splintermax92@yandex.ru>
2024-01-31 23:37:04 +0300

    tests_comments_v2

--- src/apps/comments/tests/test_api.py
+++ src/apps/comments/tests/test_api.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
-from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 
@@ -13,11 +14,13 @@ from src.apps.users.models import CustomUser
 class CommentAPITestCase(APITestCase):
     def setUp(self):
         self.supervisor = CustomUser.objects.create(
-            username="user_1", password="testpassword"
+            username="supervisor", password="testpassword"
         )
         self.supervisor.save()
         self.employee = CustomUser.objects.create(
-            username="employee", password="testpassword"
+            username="employee",
+            password="testpassword",
+            supervisor=self.supervisor,
         )
         self.employee.save()
 
@@ -35,16 +38,17 @@ class CommentAPITestCase(APITestCase):
         self.supervisor_token = Token.objects.create(user=self.supervisor)
         self.employee_token = Token.objects.create(user=self.employee)
 
+        self.url_list = reverse(
+            "api_v1:comments-list", kwargs={"task_id": self.task.id}
+        )
+
         self.data = {"text_of_comment": "Test comment text"}
 
     def test_create_comment(self):
-        client = APIClient()
-        client.credentials(
+        self.client.credentials(
             HTTP_AUTHORIZATION="Token " + self.supervisor_token.key
         )
-        response = self.client.post(
-            "/api/v1/users/lms/tasks/1/comments/".format(self.task), self.data
-        )
+        response = self.client.post(self.url_list, self.data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Comment.objects.count(), 1)
@@ -53,7 +57,68 @@ class CommentAPITestCase(APITestCase):
         )
 
     def test_create_invalid_comment(self):
-        response = self.client.post(
-            "/api/v1/users/lms/tasks/1/comments/", self.data, format="json"
-        )
+        response = self.client.post(self.url_list, self.data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_comments(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + self.supervisor_token.key
+        )
+        response = self.client.post(self.url_list)
+        Comment.objects.create(
+            text_of_comment="Comment 1",
+            task=self.task,
+            comment_author=self.supervisor,
+        )
+        Comment.objects.create(
+            text_of_comment="Comment 2",
+            task=self.task,
+            comment_author=self.supervisor,
+        )
+
+        response = self.client.get(self.url_list)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_update_comment(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + self.employee_token.key
+        )
+        comment = Comment.objects.create(
+            text_of_comment="Old text",
+            task=self.task,
+            comment_author=self.employee,
+        )
+        data = {"text_of_comment": "New text"}
+
+        response = self.client.patch(
+            "/api/v1/users/lms/tasks/{}/comments/{}/".format(
+                self.task.id, comment.id
+            ),
+            data,
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        comment.refresh_from_db()
+        self.assertEqual(comment.text_of_comment, "New text")
+
+    def test_delete_comment(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + self.supervisor_token.key
+        )
+        comment = Comment.objects.create(
+            text_of_comment="To be deleted",
+            task=self.task,
+            comment_author=self.supervisor,
+        )
+
+        response = self.client.delete(
+            "/api/v1/users/lms/tasks/{}/comments/{}/".format(
+                self.task.id, comment.id
+            )
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Comment.objects.count(), 0)
