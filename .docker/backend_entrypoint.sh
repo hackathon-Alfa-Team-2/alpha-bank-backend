2073c26bde6c71906dde605f3114baa5b0f20a25
Andey Dogadkin <jvgger@gmail.com>
2024-02-03 00:49:20 +0300

    Delete backend_entrypoint.sh.

--- .docker/backend_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while pg_isready -h db -p 5432;
-    do sleep 5;
-    echo "Ожидание базы данных"
-done;
-    echo "База данных подключена"
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
