c0ed49670233adc8854e4a831170184ff42e5c37
Andey Dogadkin <jvgger@gmail.com>
2024-03-13 18:04:40 +0300

    Upgrade conftest.py.

--- tests/pytest_tests/conftest.py
+++ tests/pytest_tests/conftest.py
@@ -1,3 +1,17 @@
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def use_dummy_cache_backend(settings):
+    """Подмена бекенда кеширования."""
+    settings.CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        }
+    }
+
+
 pytest_plugins = [
     "tests.pytest_tests.fixtures.users_fixtures",
+    "tests.pytest_tests.fixtures.lms_fixtures",
 ]
